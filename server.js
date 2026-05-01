ae770e08f4a4e94097178a4f77034d910912c8c7
DonFilu <maiqui.mgs@gmail.com>
2026-05-01 01:53:53 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -97,7 +97,9 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const MONGO_URI = process.env.MONGODB_URI;
 
-mongoose.connect(MONGO_URI)
+mongoose.connect(MONGO_URI, {
+  serverSelectionTimeoutMS: 8000
+})
 .then(() => console.log("ðŸŸ¢ Mongo conectado"))
 .catch(err => console.log("âŒ Erro Mongo:", err));
 
